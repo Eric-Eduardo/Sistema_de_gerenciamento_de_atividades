8c69f0d36236d1636fd128f917aed77c3adb8be7
Eric Eduardo <ericeduardoaa@gmail.com>
2024-06-25 02:08:17 -0300

    Adição de uma apresentação

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ import view.MainView;
 public class Main {
     public static void main(String[] args) {
         MainView mainView = new MainView();
+
+        System.out.println("\r\n" + //
+                        "┌─────────┐ ┌─────────┐ ┌───────────┐ \r\n" + //
+                        "│  ┌──────┘ │  ┌──────┘ │  ┌─────┐  │\r\n" + //
+                        "│  └──────┐ │  │┌─────┐ │  └─────┘  │\r\n" + //
+                        "└──────┐  │ │  │└──┐  │ │  ┌─────┐  │\r\n" + //
+                        "┌──────┘  │ │  └───┘  │ │  │     │  │\r\n" + //
+                        "└─────────┘ └─────────┘ └──┘     └──┘\r\n" + //
+                        "\r\n" + //
+                        "SISTEMA DE GERENCIAMENTE DE ATIVIDADES");
         mainView.startView();
     }
 }
